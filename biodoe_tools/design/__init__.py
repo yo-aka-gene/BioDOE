425abfc537b881414d244d6b8f93e98589f9c9c0
Yuji Okano <yujiokano@keio.jp>
2026-09-24 16:05:47 +0900

    :bug: fix bug in DOCLOO import error

--- biodoe_tools/design/__init__.py
+++ biodoe_tools/design/__init__.py
@@ -1,15 +1,17 @@
 from ._abstract import DOE, DesignMatrix
 from ._cloo import CLOO
 from ._d_optimization import DOptimization, d_criterion
+from ._docloo import DOCLOO
 from ._fullfact import FullFactorial
 from ._pb import PlackettBurman
 
 __all__ = [
-    CLOO,
-    DesignMatrix,
-    DOE,
-    FullFactorial,
-    PlackettBurman,
-    d_criterion,
-    DOptimization,
+    "CLOO",
+    "DesignMatrix",
+    "DOCLOO",
+    "DOE",
+    "FullFactorial",
+    "PlackettBurman",
+    "d_criterion",
+    "DOptimization",
 ]
